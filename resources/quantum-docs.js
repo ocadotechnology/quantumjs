cf3ab379a05593d777cb8385abde57ba0c69dfa3
Charlie Frater <charlie.frater@ocado.com>
2015-12-21 16:56:20 +0000

    start updating the docs

--- resources/quantum-docs.js
+++ resources/quantum-docs.js
@@ -1,27 +1,25 @@
- // function setupMenu(node) {
- //    var selection = hx.select(node).classed('docs-menu-container', true)
- //      .append('div').class('hx-content')
- //      .append('div').class('hx-group hx-horizontal')
+// function setupMenu(node) {
+//    var selection = hx.select(node).classed('docs-menu-container', true)
+//      .append('div').class('hx-content')
+//      .append('div').class('hx-group hx-horizontal')
 
- //    //XXX: turn this into part of the json request
- //    selection.append('div').class('hx-section hx-fixed')
- //      .append('div').class('hx-margin hx-pad')
- //      .add(hx.detached('h3').text('About'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/getting-started/').text('Installation / Getting Started'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/core-concepts/').text('Core concepts'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/examples/').text('Examples'))
- //      .add(hx.detached('h3').text('Transforms'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/html/').text('HTML'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/template/').text('Template'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/version/').text('Version'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/api/').text('Api'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/hexagon/').text('Version'))
- //      .add(hx.detached('h3').text('Other'))
- //        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/docs/faq/').text('FAQ'))
+//    //XXX: turn this into part of the json request
+//    selection.append('div').class('hx-section hx-fixed')
+//      .append('div').class('hx-margin hx-pad')
+//      .add(hx.detached('h3').text('About'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/getting-started/').text('Installation / Getting Started'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/core-concepts/').text('Core concepts'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/examples/').text('Examples'))
+//      .add(hx.detached('h3').text('Transforms'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/html/').text('HTML'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/template/').text('Template'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/version/').text('Version'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/api/').text('Api'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/transform/hexagon/').text('Version'))
+//      .add(hx.detached('h3').text('Other'))
+//        .add(hx.detached('a').class('docs-dropdown-link').attr('href', '/docs/faq/').text('FAQ'))
 
-
-
-function setupMenu(element) {
+function setupMenu (element) {
   hx.select(element).text('Api')
 }
 
@@ -30,12 +28,12 @@ new hx.Menu('#transforms-menu', {
     {name: 'Html',  link: '/transforms/html' },
     {name: 'Api',  link: '/transforms/api' },
     {name: 'Version',  link: '/transforms/version' },
+    {name: 'Changelog',  link: '/transforms/changelog' },
     {name: 'Template', link: '/transforms/template' }
   ],
-  renderer: function(element, data) {
+  renderer: function (element, data) {
     hx.select(element).append('a')
       .text(data.name)
       .attr('href', data.link)
   }
 })
-
