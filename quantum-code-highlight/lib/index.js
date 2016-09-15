c93523f3e595c78d8c9777019188dc1c33f467a5
James Smyth <jmsmyth21@gmail.com>
2016-09-15 09:11:05 +0100

    Fix eslint errors in code-highlight

--- quantum-code-highlight/lib/index.js
+++ quantum-code-highlight/lib/index.js
@@ -1,46 +1,47 @@
 'use strict'
+
 const hljs = require('highlight.js')
 const dom = require('quantum-dom')
 const quantumSyntax = require('./quantum-syntax.js')
 const path = require('path')
 
+const stylesheetAsset = dom.asset({
+  url: '/assets/quantum-code-highlight.css',
+  file: path.join(__dirname, '../assets/quantum-code-highlight.css'),
+  shared: true
+})
+
 // TODO: try and get quantum registered in hljs
 hljs.registerLanguage('um', quantumSyntax)
 
+function highlightCode (language, code) {
+  if (language) {
+    return hljs.highlight(language, code, true).value
+  } else {
+    return hljs.highlightAuto(code).value
+  }
+}
+
+function codeblock (selection, transform) {
+  const language = selection.ps()
+  return dom.create('div')
+    .class('quantum-code-highlight-codeblock' + (language ? ' language-' + language : ''))
+    .add(dom.create('pre').text(highlightCode(language, selection.cs()), {escape: false}))
+    .add(stylesheetAsset)
+}
+
+function code (selection, transform) {
+  const language = selection.ps()
+  return dom.create('code')
+    .class('quantum-code-highlight-code' + (language ? ' language-' + language : ''))
+    .text(highlightCode(language, selection.cs()), {escape: false})
+    .add(stylesheetAsset)
+}
+
 function transforms (opts) {
-  const highlightCode = (language, code) => {
-    if (language) {
-      return hljs.highlight(language, code, true).value
-    } else {
-      return hljs.highlightAuto(code).value
-    }
-  }
-
-  const stylesheetAsset = dom.asset({
-    url: '/assets/quantum-code-highlight.css',
-    file: path.join(__dirname, '../assets/quantum-code-highlight.css'),
-    shared: true
-  })
-
-  const codeblock = (selection, transform) => {
-    const language = selection.ps()
-    return dom.create('div')
-      .class('quantum-code-highlight-codeblock' + (language ? ' language-' + language : ''))
-      .add(dom.create('pre').text(highlightCode(language, selection.cs()), {escape: false}))
-      .add(stylesheetAsset)
-  }
-
-  const code = (selection, transform) => {
-    const language = selection.ps()
-    return dom.create('code')
-      .class('quantum-code-highlight-code' + (language ? ' language-' + language : ''))
-      .text(highlightCode(language, selection.cs()), {escape: false})
-      .add(stylesheetAsset)
-  }
-
   return Object.freeze({
-    codeblock,
-    code
+    codeblock: codeblock,
+    code: code
   })
 }
 
