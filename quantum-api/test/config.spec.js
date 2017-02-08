2e02f54411a32b1834af56b987a43760639b6fd3
Charlie Frater <charlie.frater@ocado.com>
2017-02-08 10:38:19 +0000

    update config tests

--- quantum-api/test/config.spec.js
+++ quantum-api/test/config.spec.js
@@ -1,55 +1,66 @@
-'use strict'
+describe('config', () => {
+  const should = require('chai').should()
+  const config = require('../lib/config')
 
-const chai = require('chai')
-const config = require('../lib/config')
-
-const should = chai.should()
-
-describe('config.resolve', () => {
-  it('should be fine with undefined being passed in', () => {
-    config.resolve(undefined).should.be.an.object
+  it('provides the correct things', () => {
+    config.should.be.an('object')
+    config.should.have.keys(['resolve', 'defaultIssueUrl'])
+    config.defaultIssueUrl.should.be.a('function')
+    config.resolve.should.be.a('function')
   })
 
-  it('should resolve targetVersions correctly', () => {
-    should.not.exist(config.resolve(undefined).targetVersions)
-    should.not.exist(config.resolve({}).targetVersions)
-    should.not.exist(config.resolve({targetVersions: undefined}).targetVersions)
-    config.resolve({targetVersions: []}).targetVersions.should.eql([])
-    config.resolve({targetVersions: ['0.1.0', '0.2.0']}).targetVersions
-      .should.eql(['0.1.0', '0.2.0'])
+  describe('defaultIssueUrl', () => {
+    // const { defaultIssueUrl } = config
+    it('placeholder', () => true.should.equal(false))
   })
 
-  it('should resolve languages correctly', () => {
-    config.resolve(undefined).languages.length.should.equal(2)
-    config.resolve({}).languages.length.should.equal(2)
-    config.resolve({languages: []}).languages.should.eql([])
-    config.resolve({languages: [{name: 'javascript'}, {name: 'css'}]})
-      .languages.should.eql([{name: 'javascript'}, {name: 'css'}])
-  })
+  describe('resolve', () => {
+    const { resolve } = config
+    it('is fine with undefined being passed in', () => {
+      resolve(undefined).should.be.an.object
+    })
 
-  it('should resolve changelogReverseVisibleList correctly', () => {
-    config.resolve(undefined).changelogReverseVisibleList.should.equal(false)
-    config.resolve({}).changelogReverseVisibleList.should.equal(false)
-    config.resolve({changelogReverseVisibleList: false}).changelogReverseVisibleList.should.equal(false)
-    config.resolve({changelogReverseVisibleList: true}).changelogReverseVisibleList.should.equal(true)
-  })
+    it('resolves targetVersions correctly', () => {
+      should.not.exist(resolve(undefined).targetVersions)
+      should.not.exist(resolve({}).targetVersions)
+      should.not.exist(resolve({targetVersions: undefined}).targetVersions)
+      resolve({targetVersions: []}).targetVersions.should.eql([])
+      resolve({targetVersions: ['0.1.0', '0.2.0']}).targetVersions
+        .should.eql(['0.1.0', '0.2.0'])
+    })
 
-  it('should resolve changelogGroupByApi correctly', () => {
-    config.resolve(undefined).changelogGroupByApi.should.equal(false)
-    config.resolve({}).changelogGroupByApi.should.equal(false)
-    config.resolve({changelogGroupByApi: false}).changelogGroupByApi.should.equal(false)
-    config.resolve({changelogGroupByApi: true}).changelogGroupByApi.should.equal(true)
-  })
+    it('resolves languages correctly', () => {
+      resolve(undefined).languages.length.should.equal(2)
+      resolve({}).languages.length.should.equal(2)
+      resolve({languages: []}).languages.should.eql([])
+      resolve({languages: [{name: 'javascript'}, {name: 'css'}]})
+        .languages.should.eql([{name: 'javascript'}, {name: 'css'}])
+    })
 
-  it('should resolve issueUrl correctly', () => {
-    function altIssueUrl (id) {
-      return id
-    }
+    it('resolves changelogReverseVisibleList correctly', () => {
+      resolve(undefined).changelogReverseVisibleList.should.equal(false)
+      resolve({}).changelogReverseVisibleList.should.equal(false)
+      resolve({changelogReverseVisibleList: false}).changelogReverseVisibleList.should.equal(false)
+      resolve({changelogReverseVisibleList: true}).changelogReverseVisibleList.should.equal(true)
+    })
 
-    config.resolve(undefined).issueUrl.should.equal(config.defaultIssueUrl)
-    config.resolve({}).issueUrl.should.equal(config.defaultIssueUrl)
-    config.resolve({issueUrl: altIssueUrl}).issueUrl.should.equal(altIssueUrl)
+    it('resolves changelogGroupByApi correctly', () => {
+      resolve(undefined).changelogGroupByApi.should.equal(false)
+      resolve({}).changelogGroupByApi.should.equal(false)
+      resolve({changelogGroupByApi: false}).changelogGroupByApi.should.equal(false)
+      resolve({changelogGroupByApi: true}).changelogGroupByApi.should.equal(true)
+    })
 
-    should.not.exist(config.resolve(undefined).issueUrl())
+    it('resolves issueUrl correctly', () => {
+      function altIssueUrl (id) {
+        return id
+      }
+
+      resolve(undefined).issueUrl.should.equal(config.defaultIssueUrl)
+      resolve({}).issueUrl.should.equal(config.defaultIssueUrl)
+      resolve({issueUrl: altIssueUrl}).issueUrl.should.equal(altIssueUrl)
+
+      should.not.exist(resolve(undefined).issueUrl())
+    })
   })
 })
--- quantum-api/test/index.spec.js
+++ quantum-api/test/index.spec.js
@@ -1,4 +1,5 @@
 'use strict'
 require('chai').should()
 require('./module.spec.js')
+require('./config.spec.js')
 require('./transforms.spec.js')
