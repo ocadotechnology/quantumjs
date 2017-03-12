a4d9a1fea3821ed7fb0503229f0b87ad36743b23
James Smyth <jmsmyth21@gmail.com>
2017-03-12 20:49:21 +0000

    Remove a confusing use of destructuring

--- docs/src/transforms/custom-language.js
+++ docs/src/transforms/custom-language.js
@@ -1,15 +1,6 @@
 const dom = require('quantum-dom')
 const path = require('path')
-// Import the builder components we need
-const { builders: { item, itemGroup, header, body: { description } } } = require('quantum-api')
-
-// Equivalent to:
-// const builders = require('quantum-api').builders
-// const body = builders.body
-// const item = builders.item
-// const itemGroup = builders.itemGroup
-// const header = builders.header
-// const descriptions = body.description
+const api = require('quantum-api')
 
 // Define the stylesheet asset we're using
 const assets = [
@@ -23,27 +14,27 @@ const assets = [
 const languageName = 'custom'
 
 // Create a group of @thing entities for the @custom item
-const things = itemGroup(languageName, 'thing', 'Things')
+const things = api.builders.itemGroup(languageName, 'thing', 'Things')
 
 function customDetails (selection, transforms) {
   // render the param string as the header details (e.g. for '@thing something' it would be 'something')
   return dom.create('div').text(selection.ps())
 }
 // Create the transform for the item header
-const customHeader = header('custom', customDetails)
+const customHeader = api.builders.header('custom', customDetails)
 
 // Create the @custom item transform
-const customBuilder = item({
+const customBuilder = api.builders.item({
   class: 'api-custom', // Set the class to add to the item
   header: customHeader, // Set the header renderer
-  content: [ description, things ] // Set the content to display (and the order)
+  content: [ api.builders.body.description, things ] // Set the content to display (and the order)
 })
 
 // Create the @thing item transform
-const thingBuilder = item({
+const thingBuilder = api.builders.item({
   class: 'api-thing',
   header: customHeader,
-  content: [ description ]
+  content: [ api.builders.body.description ]
 })
 
 function changelogHeader (changelogHeaders) {
