328d6d03b32e7ad9b1873edf4c2f1dc2b16a2687
Charlie Frater <charlie.frater@ocado.com>
2017-04-11 14:38:54 +0100

    improve example sections and documentation in code comment

--- docs/src/transforms/index.js
+++ docs/src/transforms/index.js
@@ -28,37 +28,48 @@ function exampleFile (selection, transformer, fileName) {
   const code = dom.create('div').class('docs-example-code-body')
     .add([
       addCodeSection('html', 'HTML'),
-      addCodeSection('um', 'Quantum'),
       addCodeSection('js', 'Javascript'),
-      addCodeSection('coffee', 'CoffeeScript'),
       addCodeSection('css', 'CSS'),
-      addCodeSection('json', 'JSON')
+      addCodeSection('json', 'JSON'),
+      addCodeSection('um', 'Quantum')
     ])
 
-  const result = selection.has('noOutput') ? undefined :
+  const output = selection.has('noOutput') ? undefined :
     selection.has('output') ?
       selection.select('output').transform(transformer) :
       selection.transform(transformer)
 
-  return { code, result }
+  const outputFile = selection.has('output') && selection.select('output').ps()
+  if (outputFile && outputFile.length) {
+    return {
+      code,
+      resultFile: {
+        head: outputFile,
+        body: output
+      }
+    }
+  } else {
+    return { code, result: output }
+  }
 }
 
 /*
 Mutli-file and language example transform
 
 @example
-  @noBackground   - Hide the background for the sections (explicitly true in @extra blocks)
-  @noOutput       - Hide all output from example (e.g remove the Example Result section)
+  @noBackground    - Hide the background for the sections (explicitly true in @extra blocks)
+  @noOutput        - Hide all output from example (e.g remove the Example Result section)
 
-  @<type>         - langauge type, displays with '<Type>' heading (e.g. @js)) can have multiple types at once
-    ...           - Code for example
+  @<type>          - langauge type, displays with '<Type>' heading (e.g. @js)) can have multiple types at once
+    ...            - Code for example
 
-  @file filename  - Define a file section, shows with <filename> (<Type>) instead of just <Type> heading
-    @noOutput     - Don't output for this file (e.g. for JS with 'require' in it)
-    @output       - Set the output of this file
-    @<type>       - Code for file
+  @file filename   - Define a file section, shows with <filename> (<Type>) instead of just <Type> heading
+    @noOutput      - Don't output for this file (e.g. for JS with 'require' in it)
+    @output        - Set the output of this file (combined with other files)
+    @output [file] - Set the output of this file (with a file name, separated from other output)
+    @@<type>       - Code for file
 
-  @output         - Set the output of this example section
+  @output          - Set the output of this example section
 
 e.g.
 @example
@@ -74,11 +85,15 @@ e.g.
 Outputs a block with rendered 'index.um' as well as codeblocks for the 'quantum.config.js' and 'index.um'
 */
 function example (selection, transformer) {
-  const { code: codeBody, result: resultBody } = !selection.has('file') ? exampleFile(selection, transformer) :
+  const { code: codeBody, result, resultFiles } = !selection.has('file') ? exampleFile(selection, transformer) :
     selection.selectAll('file').map(s => exampleFile(s, transformer, s.ps()))
-      .reduce(({ code, result }, { code: newCode, result: newResult }) => {
-        return { code: [...code, newCode], result: [...result, newResult] }
-      }, { code: [], result: [] })
+      .reduce(({ code, result, resultFiles }, { code: newCode, result: newResult, resultFile: newResultFile }) => {
+        return {
+          code: newCode ? [...code, newCode] : code,
+          result: newResult ? [...result, newResult] : result,
+          resultFiles: newResultFile ? [...resultFiles, newResultFile] : resultFiles
+        }
+      }, { code: [], result: [], resultFiles: [] })
 
   const noOutput = selection.has('noOutput')
 
@@ -87,17 +102,28 @@ function example (selection, transformer) {
     dom.create('div').class('docs-example-code').add(codeBody)
   ]
 
-  const result = noOutput ? undefined : [
-    dom.create('div').class('docs-example-heading qm-header-font').text('Example Result'),
-    dom.create('div').class('docs-example-body').add(selection.has('output') ?
+  const transformedResult = noOutput ? undefined :
+    dom.create('div').class('docs-example-body-section').add(selection.has('output') ?
       selection.select('output').transform(transformer) :
-      resultBody)
-  ]
+      result)
 
-  return dom.create('div').class('docs-example')
-      .classed('docs-example-no-background', selection.has('noBackground'))
-      .add(code)
-      .add(result)
+  const transformedResultFiles = noOutput || !resultFiles || (resultFiles && !resultFiles.length) ? undefined : resultFiles.map(({head, body}) => {
+    return dom.create('div').class('docs-example-body-section')
+      .add(dom.create('div').class('docs-example-body-heading').add(head))
+      .add(body)
+  })
+
+  const resultHead = noOutput || (!transformedResult && !transformedResultFiles) ? undefined :
+    dom.create('div').class('docs-example-heading qm-header-font').text('Example Result')
+
+  const docsExample = dom.create('div').class('docs-example')
+    .classed('docs-example-no-background', selection.has('noBackground'))
+    .add(code)
+
+  return docsExample
+    .add(resultHead)
+    .add(transformedResultFiles || transformedResult ? dom.create('div').class('docs-example-body')
+      .add(transformedResultFiles || transformedResult) : undefined)
 }
 
 function customTransform (selection, transformer) {
