2a8335e20e707de5180e51c38eff6bc3375da1bf
Charlie Frater <charlie.frater@ocado.com>
2017-05-02 09:23:34 +0100

    add heading links to qm-markdown

--- quantum-markdown/lib/index.js
+++ quantum-markdown/lib/index.js
@@ -19,16 +19,44 @@ const stylesheetAsset = dom.asset({
   shared: true
 })
 
-marked.setOptions({
-  highlight: codeHighlight.highlightCode
-})
+function headingRenderer (headings) {
+  return (text, level) => {
+    const escapedText = text.toLowerCase().replace(/[^\w]+/g, '-')
+    const duplicateIndex = headings.map(({ text }) => text).indexOf(escapedText)
+    let duplicateText = undefined
+    if (duplicateIndex === -1) {
+      headings.push({
+        text: escapedText,
+        count: 0
+      })
+    } else {
+      headings[duplicateIndex].count++
+      duplicateText = `${escapedText}-${headings[duplicateIndex].count}`
+    }
+    return `<h${level}>${text}<a class="qm-docs-anchor-icon" id="${duplicateText || escapedText}" href="#${duplicateText || escapedText}"></a></h${level}>\n`
+  }
+}
+
+function parseMarkdown (content) {
+  const renderer = new marked.Renderer()
+  renderer.heading = headingRenderer([])
+  const markdownOpts = {
+    renderer,
+    highlight: (code, language) => {
+      if (language) {
+        return codeHighlight.highlightCode(code, language)
+      } else {
+        return code
+      }
+    }
+  }
+  return marked(content, markdownOpts)
+    .replace(/<code( class="(.*)")?>/gi, '<code class="qm-code-font $2">')
+}
 
 function markdown (selection, transform) {
-  const parsedMarkdown = marked(selection.cs())
-    .split('<code>')
-    .join('<code class="qm-code-font">')
   return dom.create('div').class('qm-markdown')
-    .add(parsedMarkdown)
+    .add(parseMarkdown(selection.cs()))
     .add(stylesheetAsset)
     .add(codeHighlight.stylesheetAsset)
 }
--- quantum-markdown/test/transforms.spec.js
+++ quantum-markdown/test/transforms.spec.js
@@ -26,7 +26,34 @@ describe('transforms', () => {
 
     transforms().markdown(selection).should.eql(
       dom.create('div').class('qm-markdown')
-        .add('<h1 id="h1">H1</h1>\n<h2 id="h2">H2</h2>\n<h3 id="h3">H3</h3>\n<h4 id="h4">H4</h4>\n<h5 id="h5">H5</h5>\n<h6 id="h6">H6</h6>\n')
+        .add('<h1>H1<a class="qm-docs-anchor-icon" id="h1" href="#h1"></a></h1>\n<h2>H2<a class="qm-docs-anchor-icon" id="h2" href="#h2"></a></h2>\n<h3>H3<a class="qm-docs-anchor-icon" id="h3" href="#h3"></a></h3>\n<h4>H4<a class="qm-docs-anchor-icon" id="h4" href="#h4"></a></h4>\n<h5>H5<a class="qm-docs-anchor-icon" id="h5" href="#h5"></a></h5>\n<h6>H6<a class="qm-docs-anchor-icon" id="h6" href="#h6"></a></h6>\n')
+        .add(dom.asset({
+          url: '/quantum-markdown.css',
+          filename: path.join(__dirname, '../assets/quantum-markdown.css'),
+          shared: true
+        }))
+        .add(stylesheetAsset)
+    )
+  })
+
+  it('links duplicate headings correctly', () => {
+    const selection = quantum.select({
+      type: 'markdown',
+      params: [],
+      content: [
+        '# Heading 1',
+        '## Heading 2',
+        '### Example',
+        '## Heading 3',
+        '### Example',
+        '## Heading 4',
+        '### Example'
+      ]
+    })
+
+    transforms().markdown(selection).should.eql(
+      dom.create('div').class('qm-markdown')
+        .add('<h1>Heading 1<a class="qm-docs-anchor-icon" id="heading-1" href="#heading-1"></a></h1>\n<h2>Heading 2<a class="qm-docs-anchor-icon" id="heading-2" href="#heading-2"></a></h2>\n<h3>Example<a class="qm-docs-anchor-icon" id="example" href="#example"></a></h3>\n<h2>Heading 3<a class="qm-docs-anchor-icon" id="heading-3" href="#heading-3"></a></h2>\n<h3>Example<a class="qm-docs-anchor-icon" id="example-1" href="#example-1"></a></h3>\n<h2>Heading 4<a class="qm-docs-anchor-icon" id="heading-4" href="#heading-4"></a></h2>\n<h3>Example<a class="qm-docs-anchor-icon" id="example-2" href="#example-2"></a></h3>\n')
         .add(dom.asset({
           url: '/quantum-markdown.css',
           filename: path.join(__dirname, '../assets/quantum-markdown.css'),
@@ -50,7 +77,7 @@ describe('transforms', () => {
 
     transforms().markdown(selection).should.eql(
       dom.create('div').class('qm-markdown')
-        .add('<p><code class="qm-code-font">inline code</code></p>\n<pre><code class="lang-js"><span class="hljs-keyword">const</span> thing = <span class="hljs-number">1</span>;\n</code></pre>\n')
+        .add('<p><code class="qm-code-font ">inline code</code></p>\n<pre><code class="qm-code-font lang-js"><span class="hljs-keyword">const</span> thing = <span class="hljs-number">1</span>;\n</code></pre>\n')
         .add(dom.asset({
           url: '/quantum-markdown.css',
           filename: path.join(__dirname, '../assets/quantum-markdown.css'),
