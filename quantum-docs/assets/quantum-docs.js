e64c12acad4282f2ba686e37f5f0a556f1dc96e9
Charlie Frater <charlie.frater@ocado.com>
2017-05-17 10:18:35 +0100

    update the `@versionSelector` transform and JS asset to work without Hexagon (using a `<select>`) Resolves #118

--- quantum-docs/assets/quantum-docs.js
+++ quantum-docs/assets/quantum-docs.js
@@ -6,16 +6,34 @@ function redirectorFunction (url, current, version) {
   }
 }
 
+function createOption (version, current) {
+  const option = document.createElement('option')
+  option.value = redirectorFunction(window.location.pathname, current, version)
+  option.innerHTML = version
+  return option
+}
+
+function goToVersion (event) {
+  const url = event.target.value
+  if (url) {
+    window.location = url
+  }
+  return false
+}
+
+function getCurrentVersion (url, current, versions) {
+  const pathSplit = url.split('/').filter(x => x)
+  const pathVersion = pathSplit[pathSplit.length - 1]
+  return versions.indexOf(pathVersion) !== -1 ? pathVersion : current || versions[versions.length - 1]
+}
+
 function createDropdown (id, versions, current) {
-  new hx.Menu('#' + id, {
-    items: JSON.stringify(versions),
-    renderer: function (e, v) {
-      hx.select(e)
-        .append('a')
-        .attr('href', redirectorFunction(window.location.pathname, current, v))
-        .text(v)
-    }
-  })
+  const url = window.location.pathname
+  const currentVersion = getCurrentVersion(url, current, versions)
+  const node = document.getElementById(id)
+  versions.forEach(v => node.appendChild(createOption(v, currentVersion)))
+  node.value = redirectorFunction(window.location.pathname, currentVersion, currentVersion)
+  node.addEventListener('change', goToVersion)
 }
 
 window.quantum = window.quantum || {}
