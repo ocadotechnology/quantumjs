12ca96d458e0c280df3a783cfcc0cb0ef6162cbe
Charlie Frater <charlie.frater@ocado.com>
2017-05-19 13:04:54 +0100

    es5-ify the browser asset

--- quantum-docs/assets/quantum-docs.js
+++ quantum-docs/assets/quantum-docs.js
@@ -7,14 +7,14 @@ function redirectorFunction (url, current, version) {
 }
 
 function createOption (version, current) {
-  const option = document.createElement('option')
+  var option = document.createElement('option')
   option.value = redirectorFunction(window.location.pathname, current, version)
   option.innerHTML = version
   return option
 }
 
 function goToVersion (event) {
-  const url = event.target.value
+  var url = event.target.value
   if (url) {
     window.location = url
   }
@@ -22,16 +22,16 @@ function goToVersion (event) {
 }
 
 function getCurrentVersion (url, current, versions) {
-  const pathSplit = url.split('/').filter(x => x)
-  const pathVersion = pathSplit[pathSplit.length - 1]
+  var pathSplit = url.split('/').filter(function (x) { return x })
+  var pathVersion = pathSplit[pathSplit.length - 1]
   return versions.indexOf(pathVersion) !== -1 ? pathVersion : current || versions[versions.length - 1]
 }
 
 function createDropdown (id, versions, current) {
-  const url = window.location.pathname
-  const currentVersion = getCurrentVersion(url, current, versions)
-  const node = document.getElementById(id)
-  versions.forEach(v => node.appendChild(createOption(v, currentVersion)))
+  var url = window.location.pathname
+  var currentVersion = getCurrentVersion(url, current, versions)
+  var node = document.getElementById(id)
+  versions.forEach(function (v) { return node.appendChild(createOption(v, currentVersion)) })
   node.value = redirectorFunction(window.location.pathname, currentVersion, currentVersion)
   node.addEventListener('change', goToVersion)
 }
