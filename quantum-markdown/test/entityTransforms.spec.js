7ef2a5c9eba44a13b505cb11ac3325c41444821d
Charlie Frater <charlie.frater@ocado.com>
2017-05-23 13:13:01 +0100

    markdown: rename `transforms` -> `entityTransforms`

--- quantum-markdown/test/entityTransforms.spec.js
+++ quantum-markdown/test/entityTransforms.spec.js
@@ -1,13 +1,13 @@
-describe('transforms', () => {
+describe('entityTransforms', () => {
   const path = require('path')
   const quantum = require('quantum-js')
   const dom = require('quantum-dom')
   const { stylesheetAsset } = require('quantum-code-highlight')
-  const { transforms } = require('..')
+  const { entityTransforms } = require('..')
 
-  it('provides the correct transforms', () => {
-    transforms().should.have.keys(['markdown'])
-    transforms().markdown.should.be.a('function')
+  it('provides the correct entityTransforms', () => {
+    entityTransforms().should.have.keys(['markdown'])
+    entityTransforms().markdown.should.be.a('function')
   })
 
   it('render some basic markdown', () => {
@@ -24,7 +24,7 @@ describe('transforms', () => {
       ]
     })
 
-    transforms().markdown(selection).should.eql(
+    entityTransforms().markdown(selection).should.eql(
       dom.create('div').class('qm-markdown')
         .add('<h1 class="qm-header-font">H1<a class="qm-docs-anchor-icon" id="h1" href="#h1"></a></h1>\n<h2 class="qm-header-font">H2<a class="qm-docs-anchor-icon" id="h2" href="#h2"></a></h2>\n<h3 class="qm-header-font">H3<a class="qm-docs-anchor-icon" id="h3" href="#h3"></a></h3>\n<h4 class="qm-header-font">H4<a class="qm-docs-anchor-icon" id="h4" href="#h4"></a></h4>\n<h5 class="qm-header-font">H5<a class="qm-docs-anchor-icon" id="h5" href="#h5"></a></h5>\n<h6 class="qm-header-font">H6<a class="qm-docs-anchor-icon" id="h6" href="#h6"></a></h6>\n')
         .add(dom.asset({
